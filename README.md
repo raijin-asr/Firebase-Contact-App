35c907bb4fe5c9f0aa30e02ba29fbceb8791a1d1
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2025-10-06 21:06:13 +0545

    Day10: Add validation on name and email

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Firebase Contact App using react
     Day 7: Refactor Contact Card with Hook & Add model for update functionality
     Day 8: Update functionality and real time refresh
     Day 9: Add toast message on add,delete and update function and Search filter on search input
+    Day10: Add validation on name and email
--- src/components/AddUpdateContact.jsx
+++ src/components/AddUpdateContact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Modal from './Modal'
-import { Formik, Field, Form } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import {collection} from "firebase/firebase";
 import {db} from "../config/firebase";
+import {toast} from "react-toastify";
+import * as Yup from 'yup';
+
+const contactSchemaValidation = Yup.object().shape({
+    name:Yup.string().required("Name is required!"),
+    email:Yup.string().email("Invalid Email").required("Email is required!"),
+})
 
 const AddUpdateContact = ({isOpen, onClose, isUpdate, contact}) => {
 
@@ -30,6 +37,7 @@ const AddUpdateContact = ({isOpen, onClose, isUpdate, contact}) => {
     <div>
         <Modal isOpen={isOpen} onClose={onClose}>
             <Formik
+                validationSchema={contactSchemaValidation}
                 initialValues={isUpdate ? {
                     name: contact.name,
                     email: contact.email,
@@ -47,10 +55,16 @@ const AddUpdateContact = ({isOpen, onClose, isUpdate, contact}) => {
                     <div className='flex flex-col gap-4'>
                         <label htmlFor='name'>Name</label>
                         <Field name="name" className="h-10 border"/>
+                        <div className='text-red-500 text-xs'>
+                            <ErrorMessage name="name"/>
+                        </div>
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label htmlFor='name'>Email</label>
                         <Field name="email" className="h-10 border"/>
+                        <div className='text-red-500 text-xs'>
+                            <ErrorMessage name="email"/>
+                        </div>
                     </div>
                     <button className='bg-green px-2 py-1 border self-end'>{isUpdate? "Update": "Add"}</button>
                 </Form>
